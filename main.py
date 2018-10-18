40a0372dc378aa1e94a352e562938f61b261cfc9
MINETA HATSUNE <1029286276@example.com>
2018-10-18 15:21:41 +0900

    一応できた

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ from cvxopt import solvers
 from pylab import *
 
 
-def f(x, a, Y, X, th, N, kernel):
-    evl_sum = 0.0
-    for n in range(N):
-        evl_sum += a[n] * Y[n] * kernel(X[n], x)
-    return evl_sum - th
-
-
 def gaussian_kernel(sigma):
     return lambda x_k, x_l: np.exp(-norm(x_k - x_l) ** 2 / (2 * (sigma ** 2)))
 
@@ -30,7 +23,7 @@ def standardize(X):
     return z_score
 
 
-def draw_graph(X, Y, a, th, N, S, kernel):
+def draw_graph(classifier, X, Y, S):
     for i in range(len(X)):
         if Y[i] == 1:
             plot(X[i, 0], X[i, 1], 'rx')
@@ -51,7 +44,7 @@ def draw_graph(X, Y, a, th, N, S, kernel):
     w, h = x1.shape
     x1.resize(x1.size)
     x2.resize(x2.size)
-    Z = array([f(array([x1, x2]), a, Y, X, th, N, kernel) for (x1, x2) in zip(x1, x2)])
+    Z = array([classifier(array([x1, x2])) for (x1, x2) in zip(x1, x2)])
     x1.resize((w, h))
     x2.resize((w, h))
     Z.resize((w, h))
@@ -66,40 +59,10 @@ def draw_graph(X, Y, a, th, N, S, kernel):
     show()
 
 
-def main(args):
-    # Set data
-    kernel_type = args.kernel
-    isSoft = args.soft
-    if isSoft:
-        c = 0.5 if args.cost is None else args.cost
-    else:
-        c = np.inf
-    if kernel_type == "p":
-        d = 2 if args.param is None else args.param
-        kernel = polynomial_kernel(d)
-    elif kernel_type == "g":
-        sigma = 5.0 if args.param is None else args.param
-        kernel = gaussian_kernel(sigma)
-    else:
-        kernel = normal_kernel()
-
-    data = []
-    for line in open(args.file):
-        tmp = [int(s) for s in line.replace(",", " ").split()]
-        data.append(tmp)
-
-    N = len(data)
-    dim = len(data[0]) - 1
-
-    X = np.empty((0, dim), int)
-    Y = []
-    for i in data:
-        x_i = np.array([i[:dim]])
-        X = np.append(X, x_i, axis=0)
-        Y.append(i[dim])
-    X = standardize(X)
-
+def get_classifier(X, Y, kernel, isSoft, c):
     # Quadratic programming problem
+    N = len(X)
+    dim = len(X[0])
     P = np.zeros((N, N))
     for i in range(N):
         for j in range(N):
@@ -124,7 +87,7 @@ def main(args):
         print("[ERROR]Fail to solve quadratic programming problem. Use soft margin SVM.")
         return
     a = array(sol['x']).reshape(N)
-    print("\n----------- Lagrange multipliers -----------\n", a, "\n")
+    # print("\n----------- Lagrange multipliers -----------\n", a, "\n")
 
     # pick up index of support vector
     sv_idx = []
@@ -141,30 +104,97 @@ def main(args):
 
     # evaluate weight
     w = np.zeros(dim)
-    for n in sv_idx:
-        w += a[n] * Y[n] * X[n]
-    print("------------------ weight ------------------\n", w, "\n")
+    for i in sv_idx:
+        w += a[i] * Y[i] * X[i]
+    # print("------------------ weight ------------------\n", w, "\n")
 
     # evaluate average of threshold
     th_sum = 0
-    for n in soft_sv_idx:
+    for i in soft_sv_idx:
         temp = 0
-        for m in range(N):
-            temp += a[m] * Y[m] * kernel(X[n], X[m])
-        th_sum += (temp - Y[n])
+        for j in range(N):
+            temp += a[j] * Y[j] * kernel(X[i], X[j])
+        th_sum += (temp - Y[i])
     th = th_sum / len(soft_sv_idx)
-    print("----------------- threshold -----------------\n", th, "\n")
+    # print("----------------- threshold -----------------\n", th, "\n")
+
+    def classifier(x):
+        evl_sum = 0.0
+        for n in range(N):
+            evl_sum += a[n] * Y[n] * kernel(X[n], x)
+        return evl_sum - th
 
     if dim == 2:
-        draw_graph(X, Y, a, th, N, sv_idx, kernel)
+        draw_graph(classifier, X, Y, sv_idx)
+
+    return classifier
+
+
+def main(args):
+    # set data
+    kernel_type = args.kernel
+    isSoft = args.soft
+    if isSoft:
+        c = 0.5 if args.cost is None else args.cost
+    else:
+        c = np.inf
+
+    data = []
+    for line in open(args.file):
+        tmp = [int(s) for s in line.replace(",", " ").split()]
+        data.append(tmp)
+
+    N = len(data)
+    dim = len(data[0]) - 1
+
+    X = np.empty((0, dim), int)
+    Y = []
+    for i in data:
+        x_i = np.array([i[:dim]])
+        X = np.append(X, x_i, axis=0)
+        Y.append(i[dim])
+    X = standardize(X)
+
+    number_of_group = args.cross
+    size_of_group = N // number_of_group
+
+    param = np.arange(8, 8.2, 0.1)
+    accuracies = []
+    for p in param:
+        if kernel_type == "p":
+            kernel = polynomial_kernel(p)
+        elif kernel_type == "g":
+            kernel = gaussian_kernel(p)
+        else:
+            kernel = normal_kernel()
+
+        # cross validation
+        accuracy_sum = 0
+        for i in range(number_of_group):
+            test_X = X[i*size_of_group:(i+1)*size_of_group]
+            test_Y = Y[i*size_of_group:(i+1)*size_of_group]
+            training_X = np.vstack((X[:i*size_of_group], X[(i+1)*size_of_group:]))
+            training_Y = Y[:i * size_of_group] + Y[(i + 1) * size_of_group:]
+            classifier = get_classifier(training_X, training_Y, kernel, isSoft, c)
+            correct = 0
+            for j in range(len(test_X)):
+                result = sign(classifier(test_X[j]))
+                if result == test_Y[j]:
+                    correct += 1
+
+            accuracy_sum += correct / len(test_X)
+        accuracies.append(accuracy_sum / number_of_group)
+    print(accuracies)
+    plot(param, accuracies)
+    show()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="filename for input data", type=str)
     parser.add_argument("-k", "--kernel", help="select kernel type (g: gaussian, p: polynomial)", type=str, default="n")
-    parser.add_argument("-p", "--param", help="set parameter for kernel", type=float)
     parser.add_argument("--soft", help="use soft margin SVM", action="store_true")
     parser.add_argument("-c", "--cost", help="set regularization parameter", type=int, default=0.5)
+    parser.add_argument("--cross", help="set regularization parameter", type=int, default=1)
     parsed_args = parser.parse_args()
     main(parsed_args)
